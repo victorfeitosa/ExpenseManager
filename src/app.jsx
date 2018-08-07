f900fb5aade1cc379a847db761e9e3d8b81cd51e
Victor Feitosa <victor.klngo@gmail.com>
2018-08-06 23:17:30 -0300

    added first actions

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import ExpenseStore from './stores/store';
+import ExpenseStore from './stores/Store';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
--- src/stores/store.jsx
+++ src/stores/store.jsx
@@ -1,9 +1,43 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // Expenses
+
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = ({
+  id
+} = {}
+) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
 const expenseReducerDefaultState = [];
 const expensesReducer = (state = expenseReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    break;
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id);
+    break;
     default:
       return state;
   }
@@ -31,7 +65,16 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expense1 = store.dispatch(addExpense({description: 'One', amount: 300}));
+const expense2 = store.dispatch(addExpense({description: 'This is expense 2', amount: 200}));
+const expense3 = store.dispatch(addExpense({description: 'Exp3', amount: 100}));
+
+store.dispatch(removeExpense({id: expense1.expense.id}));
+store.dispatch(removeExpense({id: expense3.expense.id}));
 
 export default store;
 
