aae99a3a3ffd6e1a823b881ece32aeffb4d7a770
Victor Feitosa <victor.klngo@gmail.com>
2018-10-28 13:32:23 -0300

    added test db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const config = {
-  apiKey: "AIzaSyAbOp-z8YRYaFuM8Hp6xghht8mNhpRb_DI",
-  authDomain: "expense-manager-vrmf.firebaseapp.com",
-  databaseURL: "https://expense-manager-vrmf.firebaseio.com",
-  projectId: "expense-manager-vrmf",
-  storageBucket: "expense-manager-vrmf.appspot.com",
-  messagingSenderId: "831904817411"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
